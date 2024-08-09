17205061c4fdf8e57818d8b8fb4d9319e53e0bc1
Liana <lcs44@discente.ifpe.edu.br>
2024-08-09 10:20:58 -0300

    Corrigindo tela de registro de recursos

--- src/br/edu/ifpe/discente/PetLife/ui/TelaRegistroRecursos.java
+++ src/br/edu/ifpe/discente/PetLife/ui/TelaRegistroRecursos.java
@@ -1,5 +1,6 @@
 package br.edu.ifpe.discente.PetLife.ui;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -9,6 +10,14 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.JRadioButton;
 
 public class TelaRegistroRecursos extends JFrame{
 
@@ -17,6 +26,7 @@ public class TelaRegistroRecursos extends JFrame{
 	 */
 	private static final long serialVersionUID = 1L;
 	private JFrame frame;
+	private CardLayout cl_telaRegistrar; 
 
 	/**
 	 * Launch the application.
@@ -48,21 +58,64 @@ public class TelaRegistroRecursos extends JFrame{
 		setResizable(false);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setBounds(100, 100, 580, 455);
-        JPanel contentPane = new JPanel();
-        contentPane.setBackground(new Color(6, 26, 53));
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+        JPanel telaPrincipalRecursos = new JPanel();
+        telaPrincipalRecursos.setBackground(new Color(6, 26, 53));
+        telaPrincipalRecursos.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(telaPrincipalRecursos);
+        telaPrincipalRecursos.setLayout(null);
         
-        JPanel panel = new JPanel();
-        panel.setBounds(10, 11, 538, 42);
-        contentPane.add(panel);
+        JPanel containerLabelRegistroRecursos = new JPanel();
+        containerLabelRegistroRecursos.setBounds(10, 11, 544, 42);
+        telaPrincipalRecursos.add(containerLabelRegistroRecursos);
         
-        JLabel lblNewLabel = new JLabel("Regristro de recursos");
-        lblNewLabel.setFont(new Font("JetBrains Mono", Font.PLAIN, 16));
-        lblNewLabel.setIcon(new ImageIcon(TelaRegristroAdocao.class.getResource("/Imagens/racao-para-animais-de-estimacao.png")));
-        panel.add(lblNewLabel);
+        JLabel labelRegistroRecursos = new JLabel("Registro de recursos");
+        labelRegistroRecursos.setFont(new Font("JetBrains Mono", Font.PLAIN, 16));
+        labelRegistroRecursos.setIcon(new ImageIcon(TelaRegristroAdocao.class.getResource("/Imagens/racao-para-animais-de-estimacao.png")));
+        containerLabelRegistroRecursos.add(labelRegistroRecursos);
+        
 
+        cl_telaRegistrar = new CardLayout();
+        
+        JPanel telaRegistrar = new JPanel(cl_telaRegistrar);
+        JButton btnEstoque = new JButton("Estoque");
+        
+        btnEstoque.setBounds(10, 64, 251, 42);
+        btnEstoque.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		cl_telaRegistrar.show(telaRegistrar, "telaEstoque");
+        	}
+        });
+        btnEstoque.setFont(new Font("JetBrains Mono", Font.PLAIN, 16));
+        telaPrincipalRecursos.add(btnEstoque);
+        
+        JButton btnMedicamentos = new JButton("Medicamentos/Vacinas");
+        btnMedicamentos.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		cl_telaRegistrar.show(telaRegistrar, "telaMedicamentos");
+        	}
+        });
+        btnMedicamentos.setBounds(297, 64, 257, 42);
+        telaPrincipalRecursos.add(btnMedicamentos);
+        btnMedicamentos.setFont(new Font("JetBrains Mono", Font.PLAIN, 16));
+        
+        telaRegistrar.setBounds(10, 129, 544, 276);
+        telaRegistrar.setBackground(new Color(6, 26, 53));
+        telaPrincipalRecursos.add(telaRegistrar);
+        
+        JPanel homeTelaRegistrar = new JPanel();
+        telaRegistrar.add(homeTelaRegistrar, "home");
+        homeTelaRegistrar.setBackground(new Color(6, 26, 53));
+        homeTelaRegistrar.setLayout(null);
+        
+        JPanel telaEstoque = new JPanel();
+        telaRegistrar.add(telaEstoque, "telaEstoque");
+        telaEstoque.setBackground(new Color(6, 26, 53));
+        telaEstoque.setLayout(null);
+        
+        JPanel telaMedicamentos = new JPanel();
+        telaRegistrar.add(telaMedicamentos, "telaMedicamentos");
+        telaMedicamentos.setBackground(new Color(6, 26, 53));
+        telaMedicamentos.setLayout(null);
+        
 	}
-
 }
