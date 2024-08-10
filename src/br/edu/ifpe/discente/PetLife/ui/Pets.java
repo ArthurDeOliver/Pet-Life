54f1666e7e4858315cffd14a2c94de1801557944
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-08-09 21:24:50 -0300

    adicionando elementos da consulta na tela de pets

--- src/br/edu/ifpe/discente/PetLife/ui/Pets.java
+++ src/br/edu/ifpe/discente/PetLife/ui/Pets.java
@@ -26,11 +26,23 @@ import java.awt.event.MouseEvent;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.border.BevelBorder;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import java.awt.SystemColor;
 
 public class Pets extends JPanel {
 
     private static final long serialVersionUID = 1L;
     private JTable table;
+    private JTextField textFieldNomePet;
+    private JTextField textFieldTipoPet;
+    private JTextField textFieldIdadePet;
+    private JTextField textFieldRacaPet;
+    private JTextField textFieldStatusPet;
+    private List<Animais> listaDeAnimais;
+    private JLabel labelFotoPet;
+    private TabelaAnimal tabelaAnimal;
 
    
     public Pets() {
@@ -73,15 +85,15 @@ public class Pets extends JPanel {
         
         // Label escrito Pets
         JLabel petsLabel = new JLabel("Pets");
-        petsLabel.setFont(new Font("Tahoma", Font.PLAIN, 24));
-        petsLabel.setBounds(10, 11, 46, 27);
+        petsLabel.setFont(new Font("JetBrains Mono", Font.PLAIN, 24));
+        petsLabel.setBounds(10, 11, 56, 27);
         add(petsLabel);
        
 
         
         // Iniciar tabela de animais
         
-        TabelaAnimal tabelaAnimal;
+        
         
         try {
         	AnimaisService servico = new AnimaisService();
@@ -95,13 +107,42 @@ public class Pets extends JPanel {
         
         
         JScrollPane scrollPaneAnimal = new JScrollPane(tabelaAnimal.getTabela());
+        scrollPaneAnimal.addMouseListener(new MouseAdapter() {
+        	
+        	public void mouseClicked(MouseEvent e) {
+        		int linhaSelecionada = tabelaAnimal.getTabela().getSelectedRow();
+        		if (linhaSelecionada >= 0) {
+        			String nomeAnimal = (String) tabelaAnimal.getTabela().getValueAt(linhaSelecionada, 1);
+        			
+        			Animais animalSelecionado = null;
+        			for (Animais animal : listaDeAnimais) {
+        			    if (animal.getNome().equals(nomeAnimal)) {
+        			        animalSelecionado = animal;
+        			        break;
+        			    }
+        			}
+        			
+        			if (animalSelecionado != null) {
+        				textFieldNomePet.setText("Nome: " + animalSelecionado.getNome());
+        				textFieldTipoPet.setText("Tipo: " + animalSelecionado.getTipo());
+        				textFieldRacaPet.setText("Raça: " + animalSelecionado.getRaca());
+                        textFieldIdadePet.setText("Idade: " + animalSelecionado.getIdade());
+                        textFieldStatusPet.setText("Status: " + animalSelecionado.getStatus());
+                        
+                        ImageIcon foto = new ImageIcon(animalSelecionado.getFoto());
+                        labelFotoPet.setIcon(foto);
+                        
+                    }
+        		}
+        		
+        	}
+        });
         scrollPaneAnimal.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         scrollPaneAnimal.setBounds(76, 131, 309, 280); 
 
         add(scrollPaneAnimal); // adicionar a tabela
         
-       
-        
+             
     
         //filtro de animais
         JComboBox comboBoxFiltro = new JComboBox();
@@ -118,13 +159,13 @@ public class Pets extends JPanel {
         
         //botão editar
         JButton botaoEditarPets = new JButton("Editar");
-        botaoEditarPets.setBounds(493, 391, 85, 21);
+        botaoEditarPets.setBounds(493, 426, 85, 21);
         add(botaoEditarPets);
         
         
         // botão excluir
         JButton botaoExcluirPets = new JButton("Excluir");
-        botaoExcluirPets.setBounds(644, 391, 85, 21);
+        botaoExcluirPets.setBounds(644, 426, 85, 21);
         add(botaoExcluirPets);
 
         
@@ -150,6 +191,72 @@ public class Pets extends JPanel {
         	}
         });
         recarregarTabela.setBounds(215, 105, 46, 16);
-        add(recarregarTabela); // recarrega a tabela
+        add(recarregarTabela);
+        
+        JLabel labelImagemPet = new JLabel("Foto");
+        labelImagemPet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelImagemPet.setBounds(582, 106, 46, 14);
+        add(labelImagemPet);
+        
+        JLabel labelIdadePet = new JLabel("Idade");
+        labelIdadePet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelIdadePet.setBounds(628, 288, 33, 27);
+        add(labelIdadePet);
+        
+        JLabel labelNomePet = new JLabel("Nome");
+        labelNomePet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelNomePet.setBounds(421, 288, 33, 27);
+        add(labelNomePet);
+        
+        JLabel labelTipoPet = new JLabel("Tipo");
+        labelTipoPet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelTipoPet.setBounds(421, 338, 33, 27);
+        add(labelTipoPet);
+        
+        JLabel labelRacaPet = new JLabel("Raça");
+        labelRacaPet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelRacaPet.setBounds(628, 338, 33, 27);
+        add(labelRacaPet);
+        
+        JLabel labelStatusPet = new JLabel("Status");
+        labelStatusPet.setFont(new Font("Tahoma", Font.BOLD, 11));
+        labelStatusPet.setBounds(499, 384, 37, 27);
+        add(labelStatusPet);
+        
+        textFieldNomePet = new JTextField();
+        textFieldNomePet.setEditable(false);
+        textFieldNomePet.setBounds(460, 288, 105, 27);
+        add(textFieldNomePet);
+        textFieldNomePet.setColumns(10);
+        
+        textFieldTipoPet = new JTextField();
+        textFieldTipoPet.setEditable(false);
+        textFieldTipoPet.setColumns(10);
+        textFieldTipoPet.setBounds(460, 338, 105, 27);
+        add(textFieldTipoPet);
+        
+        textFieldIdadePet = new JTextField();
+        textFieldIdadePet.setEditable(false);
+        textFieldIdadePet.setColumns(10);
+        textFieldIdadePet.setBounds(667, 288, 105, 27);
+        add(textFieldIdadePet);
+        
+        textFieldRacaPet = new JTextField();
+        textFieldRacaPet.setEditable(false);
+        textFieldRacaPet.setColumns(10);
+        textFieldRacaPet.setBounds(667, 341, 105, 27);
+        add(textFieldRacaPet);
+        
+        textFieldStatusPet = new JTextField();
+        textFieldStatusPet.setEditable(false);
+        textFieldStatusPet.setColumns(10);
+        textFieldStatusPet.setBounds(546, 384, 105, 27);
+        add(textFieldStatusPet);
+        
+        labelFotoPet = new JLabel("");
+        labelFotoPet.setBounds(520, 131, 140, 140);
+        add(labelFotoPet);
+        
+
     }
 }
