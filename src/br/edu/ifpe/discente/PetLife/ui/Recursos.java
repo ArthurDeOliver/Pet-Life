0ea81f31b8aff1da42c8c87ae1dbfec4f6340762
LuizHenrique5972 <luizhonascimento@gmail.com>
2024-08-12 03:24:40 -0300

    Anotações, botão, adição de outros Jpanel pra parte de cadastro

--- src/br/edu/ifpe/discente/PetLife/ui/Recursos.java
+++ src/br/edu/ifpe/discente/PetLife/ui/Recursos.java
@@ -65,6 +65,8 @@ public class Recursos extends JPanel {
 		btnRegistrarAdocao.setBounds(812, 11, 82, 46);;
 		add(btnRegistrarAdocao);
 		
+		//Aqui vai ter as coisas da tabela
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(77, 111, 342, 288);
 		add(scrollPane);
@@ -79,10 +81,18 @@ public class Recursos extends JPanel {
 			}
 		));
 		
+		//Filtro de animais
+		
 		JComboBox comboBoxFiltro = new JComboBox();
 		comboBoxFiltro.setModel(new DefaultComboBoxModel(new String[] {"Todos", "Cachorro", "Gato"}));
 		comboBoxFiltro.setBounds(304, 84, 115, 16);
 		add(comboBoxFiltro);
+		
+		//Botão que vai extrair os relatórios
+		
+		JButton btnRelatorio = new JButton("Relatório");
+		btnRelatorio.setBounds(348, 410, 112, 23);
+		add(btnRelatorio);
 		
 	}
 }
