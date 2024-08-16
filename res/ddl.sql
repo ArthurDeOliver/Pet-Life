012dc445768d792a338c45e26dc048ec86e3e24a
Liana <luizhonascimento@gmail.com>
2024-08-16 16:00:27 -0300

    Criação das tabelas de medicamentos, ração e vacina.

--- res/ddl.sql
+++ res/ddl.sql
@@ -13,3 +13,24 @@ status VARCHAR(45),
 vacina VARCHAR(45),
 foto VARCHAR(45)
 );
+
+CREATE TABLE MEDICAMENTOS (
+Nome_medicamento VARCHAR(45),
+Quantidade INT,	
+Valor DECIMAL (10,2),
+Primary key Nome_medicamento 	
+);
+
+CREATE TABLE Ração(
+Marca_Racao VARCHAT(45),
+Quantidade_Racao INT,
+Valor_Racao DECIMAL (8,2),
+Primary key Marca_Racao		
+);
+
+CREATE TABLE Vacina(
+Nome_Vacina VARCHAR(45),
+Quantidade_Vacina INT,
+Valor_Vacina DECIMAL (10,2),	
+Primary key Nome_Vacina,
+);
